ecdb2d9a244c3b32a3dbf1f01380654850e89d22
velnatkar4 <140643419+velnatkar4@users.noreply.github.com>
2023-12-31 14:07:48 -0600

    Update databasesetup.sql

--- sfutils/databasesetup.sql
+++ sfutils/databasesetup.sql
@@ -333,3 +333,210 @@ SELECT
         where
 
             and tbl.table_name not ILIKE 'snowpark_temp_table_%';
+
+
+
+--================================================================================
+-- VWH Size Calculator for COPY Statement
+--================================================================================
+CREATE OR REPLACE transient TABLE SF_VWH_SIZE
+( 
+  VWH_NO    NUMBER,
+  VWH_SIZE  VARCHAR NOT NULL,
+  VWH_WEIGHT NUMBER NOT NULL
+);
+
+INSERT INTO SF_VWH_SIZE (VWH_NO, VWH_SIZE, VWH_WEIGHT)
+VALUES  (1,'X-Small',1),
+        (2,'Small',2),
+        (3,'Medium',4),
+        (4,'Large',8),
+        (5,'X-Large',16),
+        (6,'2X-Large',32),
+        (7,'3X-Large',64),
+        (8,'4X-Large',128),
+        (9,'5X-Large',256),
+        (10,'6X-Large',512)
+;
+
+set user_name = 'DATA_INGEST_USER';
+set days = 45;
+
+WITH CTE_COPY_HST AS
+(
+      SELECT  TABLE_SCHEMA_NAME,
+              TABLE_NAME,
+              DATE_TRUNC('SECOND', LAST_LOAD_TIME) AS LAST_LOAD_TIME_HR,              
+              SUM(FILE_SIZE) AS BYTES_FILE_SIZE_TOTAL,              
+             
+              SUM(CASE WHEN FILE_SIZE > 0 THEN FILE_SIZE/POWER(1024, 2)
+                      ELSE FILE_SIZE
+                      END) AS MB_FILE_SIZE_TOTAL,
+  
+              SUM(CASE WHEN FILE_SIZE > 0 THEN FILE_SIZE/POWER(1024, 3)
+                      ELSE FILE_SIZE
+                      END) AS GiB_FILE_SIZE_TOTAL,
+  
+              AVG(CASE WHEN FILE_SIZE > 0 THEN FILE_SIZE/POWER(1024, 2)
+                                              ELSE FILE_SIZE
+                                              END) AS AVG_MB_FILE_SIZE_TOTAL,             
+  
+              MIN(CASE WHEN FILE_SIZE > 0 THEN FILE_SIZE/POWER(1024, 2)
+                                  ELSE FILE_SIZE
+                                  END) AS MIN_MB_FILE_SIZE_TOTAL,
+
+              MAX(CASE WHEN FILE_SIZE > 0 THEN FILE_SIZE/POWER(1024, 2)
+                                  ELSE FILE_SIZE
+                                  END) AS MAX_MB_FILE_SIZE_TOTAL,
+  
+              SUM(ROW_COUNT) AS ROW_COUNT,
+              SUM(ROW_PARSED) AS ROW_PARSED,              
+              COUNT(FILE_NAME) AS FILE_COUNT      
+      FROM SNOWFLAKE.ACCOUNT_USAGE.COPY_HISTORY
+      WHERE LAST_LOAD_TIME::DATE >= CURRENT_DATE - $days      
+      GROUP BY ALL
+),
+CTE_QRY_HST AS
+(
+    SELECT  USER_NAME
+           ,DATE_TRUNC('SECOND', START_TIME) AS START_TIME_HR           
+           ,START_TIME::DATE as THE_DT
+           ,QUERY_ID           
+           ,SUBSTR(QUERY_TEXT,POSITION('.', QUERY_TEXT, 1)+1) AS TABLE_STR
+           ,UPPER(TRIM(SUBSTR(TABLE_STR,1,POSITION(' ', TABLE_STR, 1)))) AS TABLE_NAME
+           ,QUERY_TYPE
+           ,WAREHOUSE_SIZE
+           ,WAREHOUSE_NAME
+           ,EXECUTION_STATUS
+           ,EXECUTION_TIME
+           ,((EXECUTION_TIME/(60000))) AS EXEC_TIME_MIN           
+           ,ROWS_PRODUCED
+           ,ERROR_MESSAGE
+           ,BYTES_WRITTEN           
+  
+           ,CASE WHEN BYTES_WRITTEN > 0 THEN BYTES_WRITTEN/POWER(1024, 2)
+                ELSE BYTES_WRITTEN
+                END AS MB_WRITTEN
+  
+           ,CASE WHEN BYTES_WRITTEN > 0 THEN BYTES_WRITTEN/POWER(1024, 3)
+                ELSE BYTES_WRITTEN
+                END AS GiB_WRITTEN
+FROM SNOWFLAKE.ACCOUNT_USAGE.QUERY_HISTORY QH
+     WHERE START_TIME::DATE >= CURRENT_DATE - $days
+     AND USER_NAME=$user_name     
+     AND QUERY_TYPE='COPY'
+     AND EXECUTION_TIME > 0
+     AND WAREHOUSE_NAME IS NOT NULL
+     AND CLUSTER_NUMBER IS NOT NULL
+),
+CTE_VWH_ANLY AS
+(
+    SELECT a.USER_NAME                      AS USER_NAME,
+       a.START_TIME_HR                      AS START_TIME,
+       a.TABLE_NAME                         AS TABLE_NAME,       
+       a.WAREHOUSE_NAME                     AS WAREHOUSE_NAME,
+       a.WAREHOUSE_SIZE                     AS VWH_IN_USED,
+       CASE
+            WHEN a.WAREHOUSE_SIZE='X-Small' THEN 1
+            WHEN a.WAREHOUSE_SIZE='Small' THEN 2
+            WHEN a.WAREHOUSE_SIZE='Medium' THEN 3
+            WHEN a.WAREHOUSE_SIZE='Large' THEN 4
+            WHEN a.WAREHOUSE_SIZE='X-Large' THEN 5
+            WHEN a.WAREHOUSE_SIZE='2X-Large' THEN 6
+            WHEN a.WAREHOUSE_SIZE='3X-Large' THEN 7
+            WHEN a.WAREHOUSE_SIZE='4X-Large' THEN 8
+            WHEN a.WAREHOUSE_SIZE='5X-Large' THEN 9
+            WHEN a.WAREHOUSE_SIZE='6X-Large' THEN 10
+            ELSE 0
+        END AS VWH_IN_USE_NUM,
+       CASE
+            WHEN (b.FILE_COUNT <= 24 AND GREATEST(a.GiB_WRITTEN,b.GiB_FILE_SIZE_TOTAL) <= 4 AND b.AVG_MB_FILE_SIZE_TOTAL <= 250 ) THEN 1
+            WHEN ((b.FILE_COUNT <= 64 AND GREATEST(a.GiB_WRITTEN,b.GiB_FILE_SIZE_TOTAL) <= 16) AND b.AVG_MB_FILE_SIZE_TOTAL <= 250 ) THEN 2
+            WHEN ((b.FILE_COUNT <= 128 AND GREATEST(a.GiB_WRITTEN,b.GiB_FILE_SIZE_TOTAL) <= 32) AND b.AVG_MB_FILE_SIZE_TOTAL <= 500 ) THEN 3
+            WHEN ((b.FILE_COUNT <= 256 AND GREATEST(a.GiB_WRITTEN,b.GiB_FILE_SIZE_TOTAL) <= 64) AND b.AVG_MB_FILE_SIZE_TOTAL <= 750 ) THEN 4
+            WHEN ((b.FILE_COUNT <= 512 AND GREATEST(a.GiB_WRITTEN,b.GiB_FILE_SIZE_TOTAL) <= 128) AND b.AVG_MB_FILE_SIZE_TOTAL <= 750 ) THEN 5
+            WHEN ((b.FILE_COUNT <= 1024 AND GREATEST(a.GiB_WRITTEN,b.GiB_FILE_SIZE_TOTAL) <= 256) AND b.AVG_MB_FILE_SIZE_TOTAL <= 1500 ) THEN 6
+            WHEN ((GREATEST(a.GiB_WRITTEN,b.GiB_FILE_SIZE_TOTAL) > 256) AND b.AVG_MB_FILE_SIZE_TOTAL <= 750 ) THEN 7
+          ELSE
+            11
+       END                                  AS VWH_RECOMMENDED,       
+       a.GiB_WRITTEN::DECIMAL(12,2)         AS GiB_WRITTEN,
+       a.MB_WRITTEN::DECIMAL(12,2)          AS MB_WRITTEN,
+       a.ROWS_PRODUCED::INTEGER                      AS ROWS_WRITTEN,
+       b.GiB_FILE_SIZE_TOTAL::DECIMAL(12,2)       AS GiB_FILE_SIZE_TOTAL,
+       b.MB_FILE_SIZE_TOTAL::DECIMAL(12,2)        AS MB_FILE_SIZE_TOTAL,
+       b.ROW_COUNT::INTEGER                          AS ROWS_FR_FILE,
+       b.FILE_COUNT::INTEGER                         AS FILE_COUNT,
+       a.EXEC_TIME_MIN::DECIMAL(12,2)       AS EXEC_TIME_MIN,
+       a.EXECUTION_STATUS                   AS EXECUTION_STATUS,
+       a.BYTES_WRITTEN::DECIMAL(32,2)       AS BYTES_WRITTEN,
+       b.BYTES_FILE_SIZE_TOTAL::DECIMAL(32,2)     AS BYTES_FILE_SIZE_TOTAL,      
+       b.MIN_MB_FILE_SIZE_TOTAL::DECIMAL(32,2)    AS MIN_MB_FILE_SIZE_TOTAL,
+       b.MAX_MB_FILE_SIZE_TOTAL::DECIMAL(32,2)    AS MAX_MB_FILE_SIZE_TOTAL,
+       b.AVG_MB_FILE_SIZE_TOTAL::DECIMAL(32,2)    AS AVG_MB_FILE_SIZE_TOTAL,
+       a.QUERY_ID,
+       CASE WHEN  b.TABLE_NAME IS NULL THEN 'COPY_SESSION_NOT_MATCH' ELSE 'COPY_SESSION_MATCH' END AS COPY_SESSION
+FROM CTE_QRY_HST a LEFT OUTER JOIN CTE_COPY_HST b
+ON a.TABLE_NAME = b.TABLE_NAME
+AND a.START_TIME_HR = b.LAST_LOAD_TIME_HR
+WHERE b.TABLE_NAME IS NOT NULL
+),
+CTE_VWH_REC AS (
+  SELECT USER_NAME
+      ,TABLE_NAME      
+      --,round(avg(VWH_IN_USE_NUM)) as in_use_vwh
+      ,VWH_IN_USE_NUM as in_use_vwh
+      ,min(VWH_RECOMMENDED) as min_vwh_sz
+      ,max(VWH_RECOMMENDED) as max_vwh_sz
+      ,round(avg(VWH_RECOMMENDED)) as rec_vwh_sz
+      ,sum(case when VWH_RECOMMENDED = 1 then 1 else 0 end) as vwh1_cnt
+      ,sum(case when VWH_RECOMMENDED = 2 then 1 else 0 end) as vwh2_cnt
+      ,sum(case when VWH_RECOMMENDED = 3 then 1 else 0 end) as vwh3_cnt
+      ,sum(case when VWH_RECOMMENDED = 4 then 1 else 0 end) as vwh4_cnt
+      ,sum(case when VWH_RECOMMENDED = 5 then 1 else 0 end) as vwh5_cnt
+      ,sum(case when VWH_RECOMMENDED = 6 then 1 else 0 end) as vwh6_cnt
+      ,sum(case when VWH_RECOMMENDED = 7 then 1 else 0 end) as vwh7_cnt
+      ,sum(case when VWH_RECOMMENDED = 8 then 1 else 0 end) as vwh8_cnt
+      ,sum(case when VWH_RECOMMENDED = 9 then 1 else 0 end) as vwh9_cnt
+      ,sum(case when VWH_RECOMMENDED = 10 then 1 else 0 end) as vwh10_cnt
+      ,COUNT(QUERY_ID) as JOB_COUNT
+      ,MIN(START_TIME::DATE) AS MIN_DATE
+      ,MAX(START_TIME::DATE) AS MAX_DATE
+      ,AVG(MB_WRITTEN) AS AVG_MB_WRITTEN
+      ,AVG(ROWS_WRITTEN) AS AVG_ROWS_WRITTEN
+      ,AVG(ROWS_FR_FILE) AS AVG_ROWS_FR_FILE
+      ,AVG(MB_FILE_SIZE_TOTAL) AS AVG_MB_FILE_SIZE_TOTAL
+      --,AVG(ROWS_FR_FILE) AS AVG_ROWS_FR_FILE
+      ,AVG(FILE_COUNT) AS AVG_FILE_COUNT
+      ,AVG(EXEC_TIME_MIN) AS AVG_EXEC_TIME_MIN
+      ,MAX(AVG_MB_FILE_SIZE_TOTAL) AS MAX_MB_FILE_SIZE_TOTAL
+  FROM CTE_VWH_ANLY
+  GROUP BY ALL
+),
+CTE_TBL_CNT AS
+(
+   SELECT TABLE_NAME,COUNT(*) AS TABLE_CNT FROM CTE_VWH_REC GROUP BY 1    
+)
+SELECT  USER_NAME,
+        r.TABLE_NAME,
+        t.TABLE_CNT,
+        MIN_DATE,
+        MAX_DATE,        
+        w.VWH_SIZE AS VWH_IN_USE,
+        v.VWH_SIZE AS VWH_RECOM,
+        JOB_COUNT,
+        AVG_MB_WRITTEN::DECIMAL(12,2)   AS MB_WRITTEN_TOTAL,
+        AVG_MB_FILE_SIZE_TOTAL::DECIMAL(12,2) AS MB_FILE_SIZE_TOTAL,
+        MAX_MB_FILE_SIZE_TOTAL::DECIMAL(12,2) AS AVG_MB_FILE_SIZE,
+        AVG_ROWS_WRITTEN::NUMBER AS AVG_ROWS_WRITTER,
+        AVG_ROWS_FR_FILE::NUMBER AS AVG_ROWS_FR_FILE,
+        AVG_FILE_COUNT::NUMBER AS AVG_FILE_COUNT,
+        AVG_EXEC_TIME_MIN::DECIMAL(12,2) AS AVG_TIME_MIN
+    FROM CTE_VWH_REC r INNER JOIN CTE_TBL_CNT t 
+        ON r.TABLE_NAME = t.TABLE_NAME
+    LEFT OUTER JOIN SF_VWH_SIZE w
+        ON r.IN_USE_VWH = w.VWH_NO
+    LEFT OUTER JOIN SF_VWH_SIZE v
+        ON r.REC_VWH_SZ = v.VWH_NO
+GROUP BY ALL
+;
