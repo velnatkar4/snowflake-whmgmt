a5558b2b624067480fbbffed32ce29ef6007e953
velnatkar4 <140643419+velnatkar4@users.noreply.github.com>
2024-12-26 10:29:28 -0600

    Update UserRequestForm.py

--- UserRequestForm.py
+++ UserRequestForm.py
@@ -1,57 +1,75 @@
-# import packages
-import streamlit as st
+## Snowflake System Usage
+#  covers usage of all warehouses
+
 import pandas as pd
-from sfutils import snowflake_run
+import streamlit as st 
+import altair as alt
 
-class user_form:
-    def __init__(self):
-        self.title = "Warehouse User Request Form"
+WH_COLOR = 'red'
+BS_COLOR = '#3f75cc'
+ET_COLOR = '#21d13f'
+RULE_COLOR = 'red'
+POINT_COLOR = 'gray'
+WIDTH = 600
 
-    def clear_form():
-        st.session_state["rnum"] = ""
-        st.session_state["rusr"] = ""
-        st.session_state["rcom"] = ""
-        st.session_state["rres"] = ""
+data = pd.read_csv('./sfutils/usage_data.csv').reset_index()
 
-    def user_request(self):
-        st.title(self.title)
-        st.session_state["userName"] = "Input username here"
-        st.session_state["userReason"] = "Input reason here"
+## If the application is laggy when deployed, there is likely too many
+#  datapoints for the frontend to handle. In this case, reduce the density
+#  of datapoints using the following line:
+data = data.iloc[::3]
 
-        placeholder = st.empty()
-        
-        with st.form("userrequest"):
-            f1, f2 = st.columns([1, 1])
-            f3, f4 = st.columns([1, 1])
-            with f1:          
-                in_username=st.text_input("User Name", st.session_state["userName"],key="rusr")
-            with f2:               
-                in_compute=st.selectbox("Compute you like to choose?", ('Small', 'Medium', 'Large'), key="rcmpt")
-            with f3:
-                in_justfication=st.text_input("Business Justification", st.session_state["userReason"], key="rbus")
-            with f4:
-                st.write()
-                sub_request = st.form_submit_button(label="Submit")     
+base = alt.Chart(data).encode(
+    alt.X('index', axis=alt.Axis(tickSize=0, labels=False), scale=alt.Scale(nice=False)).title(None),
+    tooltip=[alt.Tooltip('EXECUTION_TIME_MIN', format='.2f', title='Execution Time (min)'),
+             alt.Tooltip('TOTAL_TB_SPILLED', format='.2f', title='TotalSpillage (TB)'),
+             alt.Tooltip('WAREHOUSE_SIZE', title='Warehouse Size')]
+)
 
-            if sub_request:
-                # retrieve the values at the time that the button is pressed           
-                if in_username == "Input username here" or in_justfication == "Input reason here" or in_compute == 'X-Small' or in_username == "" or in_username == "":
-                    st.error("Please input the required information above.")
-                else:
-                    sql = f"""INSERT INTO WH_REQUEST_TRACKER (REQUEST_USER, REQUEST_COMPUTE, REQUEST_REASON) 
-                            VALUES('{in_username.upper()}', '{in_compute}', '{in_justfication.upper()}');"""                
-                    snowflake_run.get_data(sql)
-                    
-                    ##Retrieve latest request number
-                    sql = f"""SELECT REQUEST_NUMBER FROM WH_REQUEST_TRACKER WHERE REVIEW_USER IS NULL ORDER BY 1 DESC LIMIT 1"""                
-                    df = snowflake_run.get_data(sql)
-                    for i in range(len(df)):
-                        out_request_num=df.loc[i, "REQUEST_NUMBER"]                
-                    st.success("**:blue[User Request]** : "+str(out_request_num))
-                    placeholder.empty()             
-                    st.stop()
-                    st.experimental_rerun()
-   
-if __name__ == "__main__":    
-    form1=user_form()
-    conn = form1.user_request()
+
+# Draw lines from data
+execution_time = base.mark_line(color=ET_COLOR).encode(
+    alt.Y('EXECUTION_TIME_MIN', axis=alt.Axis(titleColor=ET_COLOR, orient='left')).title('Execution Time (min)')
+)
+
+bytes_spilled = base.mark_line(stroke=BS_COLOR, interpolate='basis').encode(
+    alt.Y('TOTAL_TB_SPILLED', axis=alt.Axis(titleColor=BS_COLOR, orient='right')).title('Total Spillage (TB)')
+)
+
+warehouse_num = base.mark_circle(color=WH_COLOR, size=10).encode(
+        alt.Y('WAREHOUSE_SIZE', axis=alt.Axis(title=None, labels=False))
+)
+
+# Create a selection that chooses the nearest point & selects based on x-value
+# NOTE: 'mouseover' is required for streamlit instead of 'pointover'
+nearest = alt.selection_point(nearest=True, on="mouseover", fields=["index"], empty=False)
+
+# Draw points on the lines, and highlight based on selection
+et_points = execution_time.mark_point(color=POINT_COLOR).encode(
+    opacity=alt.condition(nearest, alt.value(1), alt.value(0)),
+)
+bs_points = bytes_spilled.mark_point(color=POINT_COLOR).encode(
+    opacity=alt.condition(nearest, alt.value(1), alt.value(0))
+)
+
+
+# Draw a rule at the location of the selection
+rules = base.mark_rule(color=RULE_COLOR).encode(
+    x="index:Q",
+    opacity=alt.condition(nearest,alt.value(1), alt.value(0)),
+).add_params(nearest)
+
+# Compile chart layers
+execution_time = alt.layer(execution_time, et_points)
+chart = alt.layer (
+    alt.layer (
+        warehouse_num,
+        bytes_spilled,
+        execution_time,
+    ).resolve_scale(y='independent'),
+    bs_points,
+    rules
+).properties(width=WIDTH)
+
+# Plot via Streamlit
+st.altair_chart(chart, use_container_width=True)
